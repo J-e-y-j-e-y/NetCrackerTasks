1ffd431708f29ff44e2570941a4067998988a75b
July <Nightmare-000@yandex.ru>
2021-07-22 21:20:48 +0400

    first

---
+++ week4/Person.java
@@ -0,0 +1,46 @@
+package week4;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Person {
+    private Calendar BDAY = null;
+
+    Person(Calendar BDAY){
+        this.BDAY = BDAY;
+    }
+
+    public static void main(String[] args) {
+        Calendar day = new GregorianCalendar();
+        day.set(2017, Calendar.FEBRUARY, 12);
+        Person p = new Person(day);
+        System.out.println(p);
+        System.out.println(p.formatDate(DatePattern.MID));
+        System.out.println(p.formatDate(DatePattern.SHORT));
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "BDAY=" + formatDate(DatePattern.FULL) +
+                '}';
+    }
+
+    String formatDate(DatePattern pattern){
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern.format);
+        return formatter.format(BDAY.getTime());
+    }
+
+    enum DatePattern{
+        FULL("d MMMM y (EEEE)"),
+        MID("dd.MM.y (E)"),
+        SHORT("dd.MM.yy");
+
+        private String format;
+
+        DatePattern(String format){
+            this.format = format;
+        }
+    }
+}
